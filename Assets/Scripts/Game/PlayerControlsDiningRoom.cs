b46d67939dc1ab99e476862e43ee4a9cb6234673
Daniel Eduardo Martinez Martinez <56524433+DanielMartinez98@users.noreply.github.com>
2026-08-25 13:53:42 -0600

    update to player controls

--- Assets/Scripts/Game/PlayerControlsDiningRoom.cs
+++ Assets/Scripts/Game/PlayerControlsDiningRoom.cs
@@ -3,12 +3,8 @@ using UnityEngine;
 public class PlayerControlsDiningRoom : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float[] cameraMaxLimits = new float[4] { -28f, 10f, -10f, 4f };
+    [SerializeField] private float[] playerMaxLimits = new float[4] { -50f, 33f, -10f, 20f };
     // Update is called once per frame
     void Update()
     {
@@ -22,16 +18,16 @@ public class PlayerControlsDiningRoom : MonoBehaviour
         ) * Time.deltaTime * 30f);
 
         Vector3 position = transform.position;
-        position.x = Mathf.Clamp(position.x, -50f, 33f);
-        position.z = Mathf.Clamp(position.z, -10f, 20f);
+        position.x = Mathf.Clamp(position.x, playerMaxLimits[0], playerMaxLimits[1]);
+        position.z = Mathf.Clamp(position.z, playerMaxLimits[2], playerMaxLimits[3]);
         transform.position = position;
 
         //the camera should follow the player but up to a limit of -29 and 29 and in the z axis it should also be locked and staggered to -34.36
 
         camera.transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, -28f, 10f),
+            Mathf.Clamp(transform.position.x, cameraMaxLimits[0], cameraMaxLimits[1]),
             camera.transform.position.y,
-            -34.36f + Mathf.Clamp(transform.position.z, -10f, 4f)
+            -34.36f + Mathf.Clamp(transform.position.z, cameraMaxLimits[2], cameraMaxLimits[3])
         );
     }
 }
